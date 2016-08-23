e28bf7c6cda902c29b2fadb3bfc00ae6625b3d68
Marie M <kimberly.m.mack@gmail.com>
2016-08-23 18:46:48 +0000

    added businesses controller

---
+++ app/controllers/businesses_controller.rb
@@ -0,0 +1,23 @@
+class ContactsController < ApplicationController
+    
+    def index
+
+    end
+    
+    def new
+
+    end
+    
+    def create
+        
+    end
+    
+    def show
+
+    end
+
+    def destroy
+
+    end
+    
+end
